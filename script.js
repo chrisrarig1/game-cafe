de1c76ec35aa9ca5f506f982ca7598d2660c55ca
Chris Rarig <cwrarig20@gmail.com>
2021-06-26 17:44:53 -0500

    java code update

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 let answer = prompt('What is your name?');
 let time = new Date().getHours();
 if(time > 12){
-    time= time - 12 + 'pm';
-} else { time = time +'am';
+    time = time - 12 + 'pm';
+} else { time = time + 'am';
 }
 let greeting = '';
 
 if(answer === 'chris' ){
     greeting = 'Hello '+ answer+'!! '+'Welcome to the Game Cafe' + time;
 }else {
-    greeting = 'Hello '+ answer+'!! '+'Welcome to the Game Cafe'+ time;
+    greeting = 'Hello '+ answer +'!! '+'Welcome to the Game Cafe'+ time;
 }
 document.write(greeting);
 
