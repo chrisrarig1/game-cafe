21d7496fa41eeb38ced7e16cc45c78f0534dce8e
Chris Rarig <cwrarig20@gmail.com>
2021-06-27 10:37:52 -0500

    added new prompts

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
 'use strict'
+let game = prompt('Do you like video games?');
+let gamer = '';
+if( game === 'yes'){
+    gamer = 'Game On!!';
+}else{
+    gamer = 'Play more video games!!';
+}
 
-let answer = prompt('What is your name?');
-let time = new Date().getHours();
+let coffee = prompt('Do you like coffee?');
+let drink = '';
+if( coffee === 'yes'){
+    drink = 'Looks like upu need more Coffee!!';
+}else{
+    drink = 'Drink more Coffee!!';
+}
+
+
+let answer = prompt('What is your name?'); 
+let time = new Date().getHours(); //15
+let timeOfDay = '';
+
+
+if(time < 12){
+    timeOfDay = 'Good Morning!!!';
+} else if(time < 17){
+    timeOfDay = 'Good Afternoon!!';
+} 
+
+else{
+    timeOfDay = 'Good Evening!!';
+}
 if(time > 12){
     time = time - 12 + 'pm';
-} else { time = time + 'am';
+} else {
+    time = time + 'am';
 }
-let greeting = '';
 
-if(answer === 'chris'){
-    greeting = 'Hello '+ answer +'!! ' +' Welcome to the Game Cafe ' + time;
-}else {
-    greeting = 'Hello '+ answer +'!! ' +' Welcome to the Game Cafe '+ time;
+let greeting = '';
+if(answer === 'Chris'){
+    greeting = timeOfDay  + ' ' + answer + " " + drink + " " + time + " " + gamer;
+} else{
+    greeting = timeOfDay  + ' ' + answer + " " + drink + " " + time + " " + gamer;
 }
+
 
 document.write(greeting);
+
+
+
+
+
+
 
 
