fbc2ff193f5c6cf16bbbc572a30b8b3ddbe8ef3c
Chris Rarig <cwrarig20@gmail.com>
2021-06-28 23:21:51 -0500

    added functions to code

--- script.js
+++ script.js
@@ -1,47 +1,73 @@
 'use strict'
-let game = prompt('Do you like video games?');
-let gamer = '';
-if( game === 'yes'){
-    gamer = 'Game On!!';
-}else{
-    gamer = 'Play more video games!!';
-}
 
-let coffee = prompt('Do you like coffee?');
-let drink = '';
-if( coffee === 'yes'){
-    drink = 'Looks like you need more Coffee!!';
-}else{
-    drink = 'Drink more Coffee!!';
-}
-
-
-let answer = prompt('What is your name?'); 
 let time = new Date().getHours(); //15
-let timeOfDay = '';
-
-
-if(time < 12){
-    timeOfDay = 'Good Morning!!!';
-} else if(time < 17){
-    timeOfDay = 'Good Afternoon!!';
-} 
-
-else{
-    timeOfDay = 'Good Evening!!';
-}
-if(time > 12){
-    time = time - 12 + 'pm';
-} else {
-    time = time + 'am';
+function getgamer(){
+    let game = prompt('Do you like video games?');
+    let gamer = '';
+    if( game === 'yes'){
+        gamer = 'Game On!!';
+    }else{
+        gamer = 'Play more video games!!';
+    }
+    return gamer;
 }
 
-let greeting = '';
-if(answer === 'Chris'){
-    greeting = timeOfDay  + ' ' + answer + " " + drink + " " + time + " " + gamer;
-} else{
-    greeting = timeOfDay  + ' ' + answer + " " + drink + " " + time + " " + gamer;
+function getcoffee(){
+    let coffee = prompt('Do you like coffee?');
+    let drink = '';
+    if( coffee === 'yes'){
+        drink = 'Looks like you need more Coffee!!';
+    }else{
+        drink = 'Drink more Coffee!!';
+    }
+    return drink;
 }
+
+function getname(){
+    let name = prompt('What is your name?'); 
+    return name;
+}
+
+
+
+
+
+function gettimeofday(time){
+    let timeOfDay = '';
+    if(time < 12){
+        timeOfDay = 'Good Morning!!!';
+    } else if(time < 17){
+        timeOfDay = 'Good Afternoon!!';
+    } 
+    
+    else{
+        timeOfDay = 'Good Evening!!';
+    }
+    return timeOfDay;
+}
+
+function getamorpm(time){
+    if(time > 12){
+        time = time - 12 + 'pm';
+    } else {
+        time = time + 'am';
+    }
+    return time;
+}
+
+function getgreeting(timeOfDay,name,drink,time,gamer){
+    let greeting = '';
+    if(name === 'Chris'){
+        greeting = timeOfDay  + ' ' + name + " " + drink + " " + time + " " + gamer;
+    } else{
+        greeting = timeOfDay  + ' ' + name + " " + drink + " " + time + " " + gamer;
+    } 
+}
+let timeOfDay = gettimeofday(time);
+let name = getname();
+let drink = getgamer();
+let time = getamorpm(time);
+let greeting = getgreeting(timeOfDay,name,drink,time,gamer)
 
 
 document.write(greeting);
